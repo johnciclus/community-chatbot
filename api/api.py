f220f1009538d1ad13fa7827a662326db0f184af
John <talent.corp@gmail.com>
2020-01-24 22:30:54 -0500

    Adding dependencies

--- api/api.py
+++ api/api.py
@@ -5,7 +5,7 @@ from resources import Message, Messages
 app = Flask(__name__)
 api = Api(app)
 api.add_resource(Messages, '/api/messages/')
-api.add_resource(Message, '/api/messages/<int:id>',endpoint='message_endpoint')
+api.add_resource(Message, '/api/messages/<int:id>', endpoint='message_endpoint')
 
 if __name__ == '__main__':
     app.run(debug=True)

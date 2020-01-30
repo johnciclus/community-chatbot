e482eb1f911cf1a5db27b823961882f2b819df54
John <talent.corp@gmail.com>
2020-01-29 23:55:41 -0500

    Defining Message and Category Models

--- api/api.py
+++ api/api.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from flask_restful import Api
 from resources import Message, Messages
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+DEBUG = True
+PORT = 5000
+HOST = "127.0.0.1"
+SQLALCHEMY_ECHO = False
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(DB_USER="user_name", DB_PASS="password", DB_ADDR="127.0.0.1", DB_NAME="messages")
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,4 +18,4 @@ api.add_resource(Messages, '/api/messages/')
 api.add_resource(Message, '/api/messages/<int:id>', endpoint='message_endpoint')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=DEBUG)
